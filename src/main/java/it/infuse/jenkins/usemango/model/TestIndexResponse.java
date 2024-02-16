1f9d445d3f3e40b95919c41382dea3ee31e098bd
Rochelle Martins <52605935+roch25@users.noreply.github.com>
2024-02-16 15:49:19 +0530

    feat: UMDEV-5612: Allow specification of environment when running tests (#1)

--- src/main/java/it/infuse/jenkins/usemango/model/TestIndexResponse.java
+++ src/main/java/it/infuse/jenkins/usemango/model/TestIndexResponse.java
@@ -2,58 +2,10 @@ package it.infuse.jenkins.usemango.model;
 
 import java.util.List;
 
-import com.google.api.client.json.GenericJson;
-import com.google.api.client.util.Key;
+public class TestIndexResponse extends PagedResponse<TestIndexItem> {
 
-public class TestIndexResponse extends GenericJson {
-
-	@Key("Items")
-	private List<TestIndexItem> items;
-	@Key("FullCount")
-	private int fullCount;
-	@Key("Offset")
-	private int offset;
-	@Key("Info")
-	private TestIndexInfo info;
-
-	/**
-	 * @return the items
-	 */
 	public List<TestIndexItem> getItems() {
-		return items;
+		return super.getItems();
 	}
 
-	/**
-	 * @return the fullCount
-	 */
-	public int getFullCount() {
-		return fullCount;
-	}
-
-	/**
-	 * @return the offset
-	 */
-	public int getOffset() {
-		return offset;
-	}
-
-	/**
-	 * @return the info
-	 */
-	public TestIndexInfo getInfo() {
-		return info;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (!(o instanceof TestIndexResponse)) {
-			return false;
-		}
-		return super.equals(o);
-	}
-
-	@Override
-	public int hashCode() {
-		return super.hashCode();
-	}
 }
